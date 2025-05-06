b1558ad2b0763c983e235eaffa694bd101d7e19a
harshita9708 <harshitahosamani04@gmail.com>
2025-05-06 15:27:27 +0530

    save

---
+++ Employee.java
@@ -0,0 +1,21 @@
+public class Employee{
+    String name;
+    int id;
+    double salary;
+    Employee(String name, int id, double salary)
+    {
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+    }
+    void displayInfo(){
+        System.out.println("Employee Id: "+id);
+        System.out.println("Employee Name: "+name);
+        System.out.println("Employee Salary: "+salary);
+    }
+    public static void main(String[] args)
+    {
+        Employee emp1= new Employee("Harshita", 100, 500000.0);
+        emp1.displayInfo();
+    }
+}
